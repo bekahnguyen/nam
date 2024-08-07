91f83a8bdb3c3c0399da15af2dbe197dfa8255c6
bekah <bekahritter@gmail.com>
2024-08-06 18:36:34 -0700

    adding new routes, trying random word generator

---
+++ src/Components/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <>
+      <h1>I'm Nam, I'm awesome</h1>
+
+      <img src="./public/myhoneylove.jpg" alt="Mr. Nam Nguyen" id="myNam"></img>
+    </>
+  );
+}
